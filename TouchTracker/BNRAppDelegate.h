7ca767c1eafbddba3c59ce44910840d91c94a033
Jan Graßegger <jan@anycook.de>
2014-05-01 02:12:45 +0200

    Initial Commit

---
+++ TouchTracker/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  TouchTracker
+//
+//  Created by Jan Graßegger on 01.05.14.
+//  Copyright (c) 2014 Jan Graßegger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TouchTracker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TouchTracker
+//
+//  Created by Jan Graßegger on 01.05.14.
+//  Copyright (c) 2014 Jan Graßegger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BNRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BNRAppDelegate class]));
+    }
+}
